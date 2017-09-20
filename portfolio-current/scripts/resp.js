80aba46fa096d82713eb78d3e75de00281a5da65
David Franklin <dave5801@gmail.com>
2017-09-19 17:21:00 -0700

    added server.js, modified gitignore

--- portfolio-current/scripts/resp.js
+++
@@ -1,8 +0,0 @@
-function respNav() {
-    var x = document.getElementById("main-nav-section");
-    if (x.className === "mainNav") {
-        x.className += " responsive";
-    } else {
-        x.className = "mainNav";
-    }
-}

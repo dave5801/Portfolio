79459421afbea3485ffe5dbd99a05cff1c77cb06
David Franklin <dave5801@gmail.com>
2017-09-25 16:44:11 -0700

    new images for responsive design, refactor of css

--- portfolio-current/public/scripts/resp.js
+++ portfolio-current/public/scripts/resp.js
@@ -1,8 +1,8 @@
 function respNav() {
-    var x = document.getElementById("main-nav-section");
-    if (x.className === "mainNav") {
-        x.className += " responsive";
-    } else {
-        x.className = "mainNav";
-    }
+  var x = document.getElementById("main-nav-section");
+  if (x.className === "mainNav") {
+    x.className += " responsive";
+  } else {
+    x.className = "mainNav";
+  }
 }

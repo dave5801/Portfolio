6e82fa861825087eecf55ff50fe1ac8bb024e630
David Franklin <dave5801@gmail.com>
2017-09-25 17:19:37 -0700

    refactoring of js, added iife, map function

--- portfolio-current/public/scripts/projects.js
+++ portfolio-current/public/scripts/projects.js
@@ -1,37 +1,40 @@
 'use strict';
 
-var projects = [];
 
-$.getJSON("scripts/projects.json", function(result){
-  $.each(result, function(i, field){
-    projects.push(field);
-  });
-  
-  result.forEach(function(projectObject) {
-    listOfProjects.push(new Project(projectObject));
-  });
+var app = app || {};
 
-  listOfProjects.forEach(function(project) {
-    $('#portfolio-projects-display').append(project.toHtml());
-  });
+(function(module){
 
-});
+  //var projects = [];
+  var listOfProjects = [];
 
-var listOfProjects = [];
+  //CONSTRUCTOR for projects
+  function Project(project){
+    Object.assign(this, project);
+  }
 
-//CONSTRUCTOR for projects
-function Project(project){
-  Object.assign(this, project);
-}
+  Project.prototype.toHtml = function(){
 
-Project.prototype.toHtml = function(){
+    var projectTemplateScript = $("#projects-helpers-template").html();
+    var projectTemplate = Handlebars.compile(projectTemplateScript);
+    var compiledProjectHtml = projectTemplate(this);
 
-  var projectTemplateScript = $("#projects-helpers-template").html();
+    return compiledProjectHtml;
 
-  var projectTemplate = Handlebars.compile(projectTemplateScript);
+  };
 
-  var compiledProjectHtml = projectTemplate(this);
+  Project.loadProjects = function(){
+    $.getJSON("scripts/projects.json", function(result){
 
-  return compiledProjectHtml;
+      listOfProjects = result.map(function(projectObj){
+        return new Project(projectObj);
+      });
 
-};
+      listOfProjects.forEach(function(project) {
+        $('#portfolio-projects-display').append(project.toHtml());
+      });
+
+    });
+  }
+  module.Project = Project;
+})(app);

1c1f3f61e34110da0d215272338686e300c5fdde
David Franklin <dave5801@gmail.com>
2018-03-01 12:27:33 -0800

    emptying portfolio for rebuild

--- portfolio-current/public/scripts/resp.js
+++
@@ -1,8 +0,0 @@
-$('#hamburger-menu').on('click', function(){
-  var x = document.getElementById("main-nav-section");
-  if (x.className === "mainNav") {
-    x.className += " responsive";
-  } else {
-    x.className = "mainNav";
-  }
-});

5196d2c259af4d2241ab8962140369f2e463642f
David Franklin <dave5801@gmail.com>
2018-03-02 13:12:09 -0800

    getting home elements together

--- portfolio-site/src/App.js
+++ portfolio-site/src/App.js
@@ -8,10 +8,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">My Awesome Personal site</h1>
+          <h2>Awesome NavBar Here</h2>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Proclaiming my Awesomeness to the World
         </p>
       </div>
     );

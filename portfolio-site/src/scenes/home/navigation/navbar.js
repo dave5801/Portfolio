a310d9b63886020690b31cb515008e812bfb20ff
David Franklin <dave5801@gmail.com>
2018-03-03 14:21:30 -0800

    experimenting with sh** navbars

--- portfolio-site/src/scenes/home/navigation/navbar.js
+++ portfolio-site/src/scenes/home/navigation/navbar.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
 import './navbar.css';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown, NavItem, MenuItem } from 'react-bootstrap';
 
 class TopNav extends Component {
   render() {
     return (
       <div className="Navbar">
         <h1>My Awesome Logo | Stuff | More Stuff</h1>
-        <Navbar>This shi** navbar</Navbar>
+        <Navbar inverse collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#brand">React-Bootstrap</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav>
+              <NavItem eventKey={1} href="#">
+                Link
+              </NavItem>
+              <NavItem eventKey={2} href="#">
+                Link
+              </NavItem>
+              <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                <MenuItem eventKey={3.1}>Action</MenuItem>
+                <MenuItem eventKey={3.2}>Another action</MenuItem>
+                <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey={3.3}>Separated link</MenuItem>
+              </NavDropdown>
+            </Nav>
+            <Nav pullRight>
+              <NavItem eventKey={1} href="#">
+                Link Right
+              </NavItem>
+              <NavItem eventKey={2} href="#">
+                Link Right
+              </NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
       </div>
     );
   }

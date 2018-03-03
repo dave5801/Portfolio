ea9f5a939534ce19733b4bdf0a81c99ec224ab9f
David Franklin <dave5801@gmail.com>
2018-03-03 15:37:42 -0800

    creating image section on home page

---
+++ portfolio-site/src/scenes/home/home-images/homeImageSection.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './homeImageSections.css';
+
+
+class HomeImageSection extends Component {
+  render() {
+    return (
+      <div className="Home-Image-Section">
+        <h1>Home Page Images Here</h1>
+      </div>
+    );
+  }
+}
+
+export default HomeImageSection;

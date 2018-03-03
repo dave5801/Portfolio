12666f72f74546050ffb6be17c1740d5520a6ece
David Franklin <dave5801@gmail.com>
2018-03-03 13:58:55 -0800

    initializing navbar

---
+++ portfolio-site/src/scenes/home/navigation/navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './navbar.css';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="Navbar">
+        <h1>Initializing Navbar</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;

904b4adac743c57e01cf4e4aa81b103c69f7b7e0
David Franklin <dave5801@gmail.com>
2018-03-03 14:48:01 -0800

    one last commit, before switching back to home

--- portfolio-site/src/scenes/home/navigation/navbar.js
+++ portfolio-site/src/scenes/home/navigation/navbar.js
@@ -1,34 +1,25 @@
 import React, { Component } from 'react';
 import './navbar.css';
-import { Navbar, Nav, NavDropdown, NavItem, MenuItem } from 'react-bootstrap';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
 class TopNav extends Component {
   render() {
     return (
       <div className="Navbar">
-        <h1>My Awesome Logo | Stuff | More Stuff</h1>
         <Navbar inverse collapseOnSelect>
           <Navbar.Header>
             <Navbar.Brand>
-              <a href="#brand">React-Bootstrap</a>
+              <a href="#brand">My Awesome Logo</a>
             </Navbar.Brand>
-            <Navbar.Toggle />
           </Navbar.Header>
           <Navbar.Collapse>
             <Nav>
               <NavItem eventKey={1} href="#">
-                Link
+                Writings
               </NavItem>
               <NavItem eventKey={2} href="#">
-                Link
+                Projects
               </NavItem>
-              <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                <MenuItem eventKey={3.1}>Action</MenuItem>
-                <MenuItem eventKey={3.2}>Another action</MenuItem>
-                <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                <MenuItem divider />
-                <MenuItem eventKey={3.3}>Separated link</MenuItem>
-              </NavDropdown>
             </Nav>
             <Nav pullRight>
               <NavItem eventKey={1} href="#">

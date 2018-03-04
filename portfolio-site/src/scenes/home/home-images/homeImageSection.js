4cf26cbecbd684c1b32283beec2eacdbac361066
David Franklin <dave5801@gmail.com>
2018-03-03 16:13:57 -0800

    placeholder images

--- portfolio-site/src/scenes/home/home-images/homeImageSection.js
+++ portfolio-site/src/scenes/home/home-images/homeImageSection.js
@@ -7,6 +7,9 @@ class HomeImageSection extends Component {
     return (
       <div className="Home-Image-Section">
         <h1>Home Page Images Here</h1>
+        <img src={ require('./images/code.png') } />
+        <img src={ require('./images/mars.png') } />
+        <img src={ require('./images/seattle.png') } />
       </div>
     );
   }

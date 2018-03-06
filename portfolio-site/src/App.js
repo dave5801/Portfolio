bd9143089b62c709bf7fb8e54039e9c3106a2e54
David Franklin <dave5801@gmail.com>
2018-03-06 14:31:13 -0800

    push to master

--- portfolio-site/src/App.js
+++ portfolio-site/src/App.js
@@ -8,13 +8,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">My Awesome Personal site</h1>
-        </header>
-        <p className="App-intro">
-          Proclaiming my Awesomeness to the World
-        </p>
         <Home></Home>
       </div>
     );
--- portfolio-site/src/scenes/home/home.js
+++ portfolio-site/src/scenes/home/home.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+ import React, { Component } from 'react';
 import './home.css';
 import Navbar from './navigation/navbar';
+import Footer from './Footer/footer';
 
 class Home extends Component {
   render() {
@@ -8,6 +9,7 @@ class Home extends Component {
       <div className="Home">
         <h1>Main Page</h1>
         <Navbar></Navbar>
+        <Footer></Footer>
         
       </div>
     );

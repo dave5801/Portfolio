da291620d6e408e828f9d26320daba364930633a
David Franklin <dave5801@gmail.com>
2018-03-07 14:47:56 -0800

    navbar ok for now

--- portfolio-site/src/scenes/home/Footer/footer.js
+++ portfolio-site/src/scenes/home/Footer/footer.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import './footer.css';
 
 class Footer extends Component {
   render() {
     return (
-        <div>
-            <h2>LinkedIn | Twitter | Github</h2>
+        <div className="Footer">
+            <p>LinkedIn | Twitter | Github</p>
         </div>
     );
   }

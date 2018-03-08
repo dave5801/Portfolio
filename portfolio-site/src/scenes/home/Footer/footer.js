0beb9d80fc79191538e823967ca3387ccfe4fc4e
David Franklin <dave5801@gmail.com>
2018-03-08 14:43:28 -0800

    refactor of Footer

--- portfolio-site/src/scenes/home/Footer/footer.js
+++ portfolio-site/src/scenes/home/Footer/footer.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import './footer.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 
 class Footer extends Component {
   render() {
     return (
+        <Router>
         <div className="Footer">
-            <p>LinkedIn | Twitter | Github</p>
+            <p>
+                <Link to="/">LinkedIn</Link>|
+                <Link to="/">Github</Link>|
+                <Link to="/">Twitter</Link>
+            </p>
         </div>
+      </Router>
     );
   }
 }

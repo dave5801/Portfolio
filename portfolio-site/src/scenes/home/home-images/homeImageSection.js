be19c1ab4de7f1d3943f5f19b79b587de2713dbd
David Franklin <dave5801@gmail.com>
2018-03-19 11:39:07 -0700

    somehow I got the photos to look cool

--- portfolio-site/src/scenes/home/home-images/homeImageSection.js
+++ portfolio-site/src/scenes/home/home-images/homeImageSection.js
@@ -5,17 +5,18 @@ const HomeImageSection = () => (
   <div className="Home-Image-Section">
     <div className="images">
         <div>
-            <h1>Image 1</h1>
-        {/*
-            <img src={ require('./images/atlantis.png') }  alt="img1"/>*/}
+
+            <img src={ require('./images/atlantis.png') }  alt="img1"/>
         </div>
 
         <div>
-            <h1>Image 2</h1>
+
+            <img src={ require('./images/mars.png') }  alt="img1"/>
         </div>
 
         <div>
-            <h1>Image 2</h1>
+
+            <img src={ require('./images/seattle.png') }  alt="img1"/>
         </div>
     </div>
   </div>
